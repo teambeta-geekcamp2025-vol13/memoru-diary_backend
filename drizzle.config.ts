6a5979d2c492105471b7b4734b93c8589408344c
rio115 <141479874+KUNI115@users.noreply.github.com>
2025-10-02 08:00:59 +0000

    add db url

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from "drizzle-kit";
-
+import process from "node:process";
 export default defineConfig({
   dialect: 'postgresql', // 'mysql' | 'sqlite' | 'turso'
   schema: './src/DB/schema',
   out: './drizzle',
+  casing: 'snake_case',
+  dbCredentials: {
+       url: process.env.DATABASE_URL!,
+    },
 })
